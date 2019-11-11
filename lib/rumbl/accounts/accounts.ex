86d85f9f5260450e9b1713668717c9cc952673b8
Blake Dietz <blakedietz@gmail.com>
2019-11-11 13:03:24 -0700

    Up to page 66

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -2,23 +2,22 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
   """
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Ally", username: "ally_cat"},
-      %User{id: "2", name: "Blake", username: "virtu"},
-      %User{id: "3", name: "Velo", username: "dog"},
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
 end
