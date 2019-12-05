e9fdeec97fea47c8635e8bc17bb660f966ec3ac7
Blake Dietz <blakedietz@gmail.com>
2019-12-05 15:16:28 -0700

    up to 77

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -5,6 +5,12 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
   def get_user(id) do
     Repo.get(User, id)
   end
